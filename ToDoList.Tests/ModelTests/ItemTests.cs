c6eb0a58bdede86dcc74808011a6ac05a3adc238
Amy Young <youngamy1223@gmail.com>
2022-02-04 20:24:32 -0700

    add 2nd passing test for Item(description)

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -10,8 +10,20 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      //Act
+      string result = newItem.Description;
+      //Assert
+      Assert.AreEqual(description, result);
     }
 
   }
