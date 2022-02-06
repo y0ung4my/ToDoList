d58ebb8ffb2f6a3f63c7878ef0d1454933681f3a
Amy Young <youngamy1223@gmail.com>
2022-02-06 12:29:21 -0700

    add user interface Co-authored by:  Wajma Niazi  <w1niazi@hotmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Collections.Generic;
+using ToDoList.Models;
 
-class Program
-{
-  static void Main()
+namespace ToDoList {
+  class Program
   {
-    // program code goes here
+    static void Main()
+    {
+      Console.WriteLine("Would you like to add an item or view the list? ['add' or 'view']");
+      string answer = Console.ReadLine();
+      if (answer == "add")
+      {
+        Console.WriteLine("What would you like to add?"); 
+        string description = Console.ReadLine();
+        Item newItem =new Item (description);
+        List<Item> newList = new List<Item> {newItem};
+
+        Main();
+      }
+      else 
+      {
+      List<Item> result = Item.GetAll();
+        foreach (Item thisItem in result)
+        {
+          Console.WriteLine(thisItem.Description); //Comment out the dispose method to see the output!
+        }
+      }
+    }
   }
 }
